22fd35329df38afea1e5f5522c51a1315854962e
Emily Horsman <me@emilyhorsman.com>
2016-11-08 13:44:24 -0500

    Significant progress on parsing

--- Main.elm
+++ Main.elm
@@ -1,11 +1,72 @@
 module Main exposing (..)
 
 import GraphicSVG exposing (..)
-import Array
+import String
+import List
+import Result
+import Maybe
+import Debug exposing (log)
 
 
 chart t =
     text (toString t) |> filled black
+
+
+type alias DataGroup = List AverageIncomeRow
+
+
+type alias AverageIncomeRow =
+    { identity : Maybe String
+    , women : Maybe Int
+    , men : Maybe Int
+    }
+
+type alias ParsedLine = (Maybe String, Maybe Int)
+
+
+group : List Int -> List a -> List (List a)
+group groupQuantities items =
+    let
+        group' quantity = List.take quantity items
+    in
+        List.map group' groupQuantities
+
+parseLine : String -> ParsedLine
+parseLine row =
+    let
+        elements = String.split "," row
+        toInt str = Just (String.toInt str |> Result.withDefault 0)
+        a = List.take 1 elements |> List.head
+        b = List.take 1 elements |> List.head
+    in
+        log "split" (Maybe.map String.trim a, Maybe.andThen b toInt)
+
+combine : List ParsedLine -> List ParsedLine -> DataGroup
+combine a b =
+    let
+        combine' lineA lineB =
+            let
+                (identity,women) = lineA
+                (_,men) = lineB
+            in
+                log "parsed" (AverageIncomeRow identity women men)
+    in
+        List.map2 combine' a b
+
+
+massage : String -> List String
+massage data =
+    String.trim data
+    |> String.lines
+
+
+parse : List Int -> String -> String -> List DataGroup
+parse groupQuantities women men =
+    let
+        groupedWomen = massage women |> List.map parseLine |> group groupQuantities
+        groupedMen = massage men |> List.map parseLine |> group groupQuantities
+    in
+        List.map2 combine groupedWomen groupedMen
 
 
 type Message
@@ -34,14 +95,16 @@ update message model =
             { model | t = tick }
 
 
+data =
+    parse [2, 2, 6] women men
+
+
 women =
     """
 Aboriginal identity,30042
 Non-Aboriginal identity,36992
-,
 Total visible minority population,33550
 Not a visible minority,37902
-,
 Non-immigrants,37154
 Immigrants,36452
     Before 1981,42842
@@ -55,10 +118,8 @@ men =
     """
 Aboriginal identity,37910
 Non-Aboriginal identity,52238
-,
 Total visible minority population,44819
 Not a visible minority,54136
-,
 Non-immigrants,52268
 Immigrants,51317
     Before 1981,60373
